69e8aacaa330dc818c16bc7be7ac0d551c1347ae
Josh Long <josh@joshlong.com>
2022-03-24 22:15:31 -0700

    schema

--- src/main/java/com/example/yugabyteftw/YugabyteftwApplication.java
+++ src/main/java/com/example/yugabyteftw/YugabyteftwApplication.java
@@ -8,71 +8,48 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.reactive.TransactionalOperator;
 import org.springframework.util.Assert;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
-
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class YugabyteftwApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(YugabyteftwApplication.class, args);
-        Thread.sleep(1000);
     }
 
     @Bean
-    ApplicationRunner runner(CustomerRepository repository,
-                             CustomerService service) {
-        return args -> service.reset("Karthik", "Bill", "Amey", "Josh")
+    ApplicationRunner runner(CustomerService service) {
+        return args -> service
+                .reset("A", "B", "C", "D", "E")
+                .doOnNext(c -> Assert.isTrue(Character.isUpperCase(c.name().charAt(0)), () -> "the first letter must be uppercase!"))
                 .subscribe(System.out::println);
     }
 
-    @Bean
-    RouterFunction<ServerResponse> routes(CustomerRepository repository) {
-        return route()
-                .GET("/customers", request -> ok().body(repository.findAll(), Customer.class))
-                .build();
-    }
 }
 
 @Service
 class CustomerService {
 
-    private final TransactionalOperator transactionalOperator;
-
     private final CustomerRepository customerRepository;
 
-    CustomerService(TransactionalOperator transactionalOperator, CustomerRepository customerRepository) {
-        this.transactionalOperator = transactionalOperator;
+    CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     @Transactional
     public Flux<Customer> reset(String... names) {
-        return this.customerRepository //
-                .deleteAll() //
-                .thenMany(Flux
-                        .fromArray(names)
-                        .map(name -> new Customer(null, name))
-                        .flatMap(this.customerRepository::save)
-                )
-                .doOnNext(c -> Assert.isTrue(Character.isUpperCase(c.name().charAt(0)), () -> "the name should start with a capital letter"));
-
+        var delete = this.customerRepository.deleteAll();
+        var write = Flux.fromArray(names)
+                .map(name -> new Customer(null, name))
+                .flatMap(this.customerRepository::save);
+        return delete.thenMany(write);
     }
 
+}
 
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
 }
 
 record Customer(@Id Integer id, String name) {
 }
-
-
-interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
-
-}
-
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,4 +3,3 @@ create table if not exists customer
     id   serial primary key,
     name varchar(255) not null
 );
-
